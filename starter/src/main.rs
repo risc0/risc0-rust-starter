218f8ef53fa63b6f0706ce56030f10e0f2855940
Frank Laub <github@frank.laub.io>
2022-06-13 12:29:57 -0700

    Update to risc0 v0.9.0 (#4)

--- starter/src/main.rs
+++ starter/src/main.rs
@@ -1,29 +1,15 @@
-use std::fs;
-
 use methods::{MULTIPLY_ID, MULTIPLY_PATH};
 use risc0_zkvm_host::Prover;
 use risc0_zkvm_serde::{from_slice, to_vec};
-use tempfile::tempdir;
 
 fn main() {
     // Pick two numbers
     let a: u64 = 17;
     let b: u64 = 23;
 
-    // Write the ID to a file, this is to work around the fact that the C++
-    // prover API doesn't take IDs as buffers currently.
-    let temp_dir = tempdir().unwrap();
-    let id_path = temp_dir
-        .path()
-        .join("multiply.id")
-        .to_str()
-        .unwrap()
-        .to_string();
-    fs::write(&id_path, MULTIPLY_ID).unwrap();
-
     // Multiply them inside the ZKP
     // First, we make the prover, loading the 'multiply' method
-    let mut prover = Prover::new(&MULTIPLY_PATH, &id_path).unwrap();
+    let mut prover = Prover::new(&MULTIPLY_PATH, MULTIPLY_ID).unwrap();
     // Next we send a + b to the guest
     prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
     prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
@@ -39,5 +25,5 @@ fn main() {
     // Here is where one would send 'receipt' over the network...
 
     // Verify receipt, panic if it's wrong
-    receipt.verify(&id_path).unwrap();
+    receipt.verify(MULTIPLY_ID).unwrap();
 }
