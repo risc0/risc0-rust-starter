3aaa4f3d16119e6014bd574baa20c6440f4a60bf
Rami Lukata <32602478+rlukata@users.noreply.github.com>
2022-07-21 16:25:54 -0700

    Update multiply.rs

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -7,15 +7,15 @@ risc0_zkvm_guest::entry!(main);
 
 pub fn main() {
     // Load the first number from the host
-    let a: u64 = env::read();
+    let a: u32 = env::read();
     // Load the second number from the host
-    let b: u64 = env::read();
+    let b: u32 = env::read();
     // Verify that neither of them are 1 (i.e. nontrivial factors)
     if a == 1 || b == 1 {
         panic!("Trivial factors")
     }
     // Compute the product
-    let c: u64 = a * b;
+    let c: u64 = (a * b).into();
     // Commit it to the public journal
     env::commit(&c);
 }
