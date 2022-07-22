ade573ebda88b59c8946be9f31f7291d3dc56485
Rami Lukata <32602478+rlukata@users.noreply.github.com>
2022-07-22 13:13:35 -0700

    restore u64 types but keep except

--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -7,14 +7,14 @@ risc0_zkvm_guest::entry!(main);
 
 pub fn main() {
     // Load the first number from the host
-    let a: u32 = env::read();
+    let a: u64 = env::read();
     // Load the second number from the host
-    let b: u32 = env::read();
+    let b: u64 = env::read();
     // Verify that neither of them are 1 (i.e. nontrivial factors)
     if a == 1 || b == 1 {
         panic!("Trivial factors")
     }
     // Compute the product while being careful with integer overflow
-    let product = (a as u64).checked_mul(b as u64).expect("Integer overflow");
+    let product = a.checked_mul(b).expect("Integer overflow");
     env::commit(&product);
 }
