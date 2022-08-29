f148082992e0aed0158e3f74927674eb0d05b2d5
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2022-08-29 18:42:37 +0200

    tests in guest environment

--- methods/build.rs
+++ methods/build.rs
@@ -1,3 +1,11 @@
+use std::collections::HashMap;
 fn main() {
-    risc0_build::embed_methods();
+    let inner_pkg_options= risc0_build::GuestOptions {
+        code_limit: 10,
+        features: vec![],
+        test_mode: true,
+    };
+
+    let map = HashMap::from([("methods-guest", inner_pkg_options)]);
+    risc0_build::embed_methods_with_options(map);
 }
--- methods/guest/src/bin/multiply.rs
+++ methods/guest/src/bin/multiply.rs
@@ -1,8 +1,27 @@
 #![no_main]
-#![no_std]
-
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 use risc0_zkvm_guest::env;
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+#[cfg(test)]
+risc0_zkvm_guest::entry!(test_main);
+
+#[cfg(not(test))]
 risc0_zkvm_guest::entry!(main);
 
 pub fn main() {
