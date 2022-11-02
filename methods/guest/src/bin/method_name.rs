1641d5f956a9355e3831fee97f9c2fd22d1b3673
Tim Zerrell <tim.zerrell@risczero.com>
2022-11-02 10:28:03 -0700

    Switch to empty project template (#20)
    * Switch to empty project template
    
    * Add a note about changing filename
    
    * Update README for the new template structure

---
+++ methods/guest/src/bin/method_name.rs
@@ -0,0 +1,10 @@
+// TODO: Rename this file to change the name of this method from METHOD_NAME
+
+#![no_main]
+#![no_std]
+
+risc0_zkvm_guest::entry!(main);
+
+pub fn main() {
+    // TODO: Implement your guest code here
+}
