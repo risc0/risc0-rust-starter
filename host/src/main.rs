705ee750e5ae06681ae2424636b8fc666ff39912
Tim Zerrell <tim.zerrell@risczero.com>
2022-11-30 23:18:05 -0800

    Format

--- host/src/main.rs
+++ host/src/main.rs
@@ -7,8 +7,9 @@ fn main() {
     // Make the prover.
     let method_code = std::fs::read(METHOD_NAME_PATH)
         .expect("Method code should be present at the specified path; did you use the correct *_PATH constant?");
-    let mut prover = Prover::new(&method_code, METHOD_NAME_ID)
-        .expect("Prover should be constructed from valid method source code and corresponding method ID");
+    let mut prover = Prover::new(&method_code, METHOD_NAME_ID).expect(
+        "Prover should be constructed from valid method source code and corresponding method ID",
+    );
 
     // TODO: Implement communication with the guest here
 
@@ -17,8 +18,9 @@ fn main() {
         .expect("Valid code should be provable if it doesn't overflow the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
 
     // Optional: Verify receipt to confirm that recipients will also be able to verify your receipt
-    receipt.verify(METHOD_NAME_ID)
-        .expect("Code you have proven should successfully verify; did you specify the correct method ID?");
+    receipt.verify(METHOD_NAME_ID).expect(
+        "Code you have proven should successfully verify; did you specify the correct method ID?",
+    );
 
     // TODO: Implement code for transmitting or serializing the receipt for other parties to verify here
 }
