e980adc1f5f9439a1b9b04486a212f50c2da286d
Tim Zerrell <tim.zerrell@risczero.com>
2022-12-03 17:59:14 -0800

    Restore no_std

--- methods/guest/src/bin/method_name.rs
+++ methods/guest/src/bin/method_name.rs
@@ -1,6 +1,7 @@
 // TODO: Rename this file to change the name of this method from METHOD_NAME
 
 #![no_main]
+#![no_std]  // std support is experimental, but you can remove this to try it
 
 risc0_zkvm_guest::entry!(main);
 
