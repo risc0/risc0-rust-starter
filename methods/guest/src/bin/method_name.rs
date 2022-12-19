921fba6f8ae22ccb9c6e5378ee63e280fad2a866
Tim Zerrell <tim.zerrell@risczero.com>
2022-12-19 15:59:56 -0800

    Switch risc0 dependency to HEAD

--- methods/guest/src/bin/method_name.rs
+++ methods/guest/src/bin/method_name.rs
@@ -3,7 +3,7 @@
 #![no_main]
 #![no_std]  // std support is experimental, but you can remove this to try it
 
-risc0_zkvm_guest::entry!(main);
+risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
     // TODO: Implement your guest code here
